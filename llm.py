78242f094c3e2c85439d680a4127019062ecf9c0
krrishchawla <krrishchawla1@gmail.com>
2024-01-13 15:43:19 -0500

    changes, prompt.py

--- llm.py
+++ llm.py
@@ -7,7 +7,7 @@ GPT4 = "gpt-4"
 GPT4_turbo = 'gpt-4-1106-preview'
 
 class GPT4QAModel():
-    def __init__(self, model=GPT4_turbo):
+    def __init__(self, model=GPT4):
         openai.api_key = os.environ.get('OPENAI_API_KEY')
         self.model = model
 
