37b892a3a9b51fdee0fee12bc1d4711aee840479
krrishchawla <krrishchawla1@gmail.com>
2024-01-13 14:09:59 -0500

    updates

--- util.py
+++ util.py
@@ -3,14 +3,6 @@ from llm import GPT4QAModel
 from PyPDF2 import PdfReader
 
 
-def generate_cover(resume, job_description):
-    # Your email generation logic here
-    model = GPT4QAModel()
-    prompt = get_prompt(resume, job_description)
-    response = model.answer_question(prompt)
-    return response
-
-
 def extract_text_from_pdf(pdf_file):
     pdf_text = ""
     pdf_reader = PdfReader(pdf_file)
